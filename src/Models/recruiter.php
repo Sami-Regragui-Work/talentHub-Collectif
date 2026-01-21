ec15cdfbf9d8f7723278dc802f66a3db0e85aa6e
mohamed <mojahdontheway@gmail.com>
2026-01-21 10:31:36 +0100

    add all models

---
+++ src/Models/recruiter.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class recruiter {
+    private int $id;
+    private string $companyName;
+
+    public function __construct( $id,$companyName ){
+        $this->id = $id;
+        $this->companyName = $companyName;
+    }
+
+    //getters 
+    public function getId(): int {
+        return $this-> id;
+
+    }
+
+    public function getCompanyName(): string {
+        return $this->companyName;
+    }
+
+    // setters 
+     
+     public function setId($id): void {
+        $this-> id = $id;
+
+    }
+
+    public function setCompanyName($companyName): void {
+        $this->companyName = $companyName;
+    }
+
+
+}
