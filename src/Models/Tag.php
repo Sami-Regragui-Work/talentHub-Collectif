5af70b486571432011ed2ceb45ffda8285f4f050
mohamed <mojahdontheway@gmail.com>
2026-01-22 12:32:46 +0100

    feat :  add tag model

--- src/Models/Tag.php
+++ src/Models/Tag.php
@@ -1,23 +1,20 @@
 <?php
 
+namespace App\Models;
+
 class Tag
 {
-    private string $name;
+    private readonly string $name; 
 
-    public function __construct(string $name)
+    public function __construct(array $data)
     {
-        $this->name = $name ;
+        $this->name = (string) $data['name'];
     }
 
-    
     public function getName(): string
     {
         return $this->name;
     }
 
-
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
+  
 }
